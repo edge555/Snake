c131502211cb95a9ac789cafb52f15dca85ae001
Shoaib Ahmed <shoaib27@student.sust.edu>
2019-06-14 03:22:21 +0600

    Updated

--- Snake.py
+++ Snake.py
@@ -9,15 +9,23 @@ width = 600
 height = 600
 window = pygame.display.set_mode((width,height))
 
+start_x = 50
+start_y = 20
+snake_width = 10
+
 pygame.display.set_caption("Snake")
 pygame.display.update()
 exit = False
 over = False
-while True:
+while not exit:
     for event in pygame.event.get():
-        print(event)
+        #print(event)
+        if event.type == pygame.QUIT:
+            exit = True
+
     window.fill(BLACK)
+    pygame.draw.rect(window,WHITE,[start_x, start_y, snake_width, snake_width])
     pygame.display.update()
 
-#pygame.quit()
+pygame.quit()
 
